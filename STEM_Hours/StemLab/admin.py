31f0925ac2976e7b372f20e49be2b27b97a463c4
Ryan <ryan.opitz@mycc.caspercollege.edu>
2023-12-01 02:23:07 -0700

    Updated admin.py
    I solved the problem of 'TutorShift' model class not appearing on the admin page. Also did some minor renaming in 'models.py'.

--- STEM_Hours/StemLab/admin.py
+++ STEM_Hours/StemLab/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import TutorShift
+from StemLab.models import TutorShift
+admin.site.register(TutorShift)
 
 class TutorAdmin(admin.ModelAdmin):
     list_display = ['day', 'start_time', 'end_time', 'notes']
--- STEM_Hours/StemLab/models.py
+++ STEM_Hours/StemLab/models.py
@@ -4,8 +4,8 @@ class TutorShift(models.Model):
     day = models.DateField('Tutoring day', help_text='Tutoring day')
     start_time = models.TimeField('Start time', help_text='Start time')
     end_time = models.TimeField('End time', help_text='End time')
-    notes = models.TextField('Textual Notes', help_text='Textual Notes', blank=True, null=True)
+    notes = models.TextField('Notes', help_text='Notes', blank=True, null=True)
 
     class Meta:
-        verbose_name = 'Scheduling'
-        verbose_name_plural = 'Scheduling'
+        verbose_name = 'Tutoring'
+        verbose_name_plural = 'Tutoring'
