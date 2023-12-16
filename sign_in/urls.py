0e03d2666d0beabaf7e3a4b16cb2afec3b2e71b1
Ryan <106790344+RyanO-Projects@users.noreply.github.com>
2023-12-16 11:46:26 -0700

    Delete sign_in directory
    replaced with accounts app

--- sign_in/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'sign_in'
-urlpatterns = [
-    path("sign_in/", views.Sign_In_View, name="sign_in"),
-    path("sign_out/", views.Sign_Out_View, name="sign_out"),
-]
